483fc973c50d9579d9cffb9fb43823d090f30e92
BrotherApollo <cshicks96@gmail.com>
2025-10-27 20:16:18 -0400

    removing unit tests

---
+++ src/app/chord-trainer/chord-trainer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chord-trainer',
+  imports: [],
+  templateUrl: './chord-trainer.html',
+  styleUrl: './chord-trainer.css'
+})
+export class ChordTrainer {
+
+}
